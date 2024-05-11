20e18b17cd07755f6547375a3f40fb3af054c4fb
Mohd Afri <mohammadsaeedafri9@gmail.com>
2024-05-11 11:10:18 +0530

    Vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,21 @@
-// import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": "backend-production-fada0.up.railway.app", // Replace with your actual backend URL
-//     },
-//   },
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        // No trailing slash here
-        // target: "http://localhost:3000/api", // Replace with your actual backend URL
-        target: "https://backend-production-fada0.up.railway.app/api",
-        changeOrigin: true, // Ensures correct headers are sent
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove '/api' prefix before proxying
+export default ({ command }) => {
+  const isProduction = command === "build";
+
+  return defineConfig({
+    server: {
+      proxy: {
+        "/api": {
+          target: isProduction
+            ? "https://backend-production-fada0.up.railway.app/api"
+            : "http://localhost:3000/api", // Use local URL during development
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+  });
+};
